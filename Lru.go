c13bcc8c47c0c89da65d5dfea633c099de978eb6
赵昊 <zhaohao.9426@bytedance.com>
2022-10-15 22:04:33 +0800

    LRU RWMutex

--- Lru.go
+++ Lru.go
@@ -1,11 +1,15 @@
 package LRU
 
-import "container/list"
+import (
+	"container/list"
+	"sync"
+)
 
 type LRUCache struct {
 	Cap  int
 	Map  map[int]*list.Element
 	List *list.List
+	Lock sync.RWMutex
 }
 
 type pair struct {
@@ -21,6 +25,9 @@ func Constructor(capacity int) LRUCache {
 }
 
 func (c *LRUCache) Get(key int) int {
+	// 需移动头部节点，因此加写锁
+	c.Lock.Lock()
+	defer c.Lock.Unlock()
 	if el, ok := c.Map[key]; ok {
 		c.List.MoveToFront(el)
 		return el.Value.(pair).V
@@ -29,6 +36,8 @@ func (c *LRUCache) Get(key int) int {
 }
 
 func (c *LRUCache) Put(key int, value int) {
+	c.Lock.Lock()
+	defer c.Lock.Unlock()
 	if el, ok := c.Map[key]; ok {
 		el.Value = pair{K: key, V: value}
 		c.List.MoveToFront(el)
