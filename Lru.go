9a21c55bab9920997a3c0aac5c974f672ce3b35f
赵昊 <zhaohao.9426@bytedance.com>
2022-10-16 11:12:58 +0800

    use hash shard to improve concurrency

--- Lru.go
+++ Lru.go
@@ -2,14 +2,31 @@ package LRU
 
 import (
 	"container/list"
+	"runtime"
 	"sync"
 )
 
-type LRUCache struct {
+/*
+doc1: https://halfrost.com/lru_lfu_interview/#toc-1
+doc2: https://carlclone.github.io/algos/lru-cache-go/
+thread safe: use Read/Write Lock on Get() / Put() / Size()
+high concurrency : use sharding , noted that this code won't pass the leetcode test ,
+the reason is the capacity of this cache is approximately , not absolutely , eviction won't act as leetcode expect
+
+for example , concurrency=3 , approximate capacity=7 , 7/3=2 , 2 segments , every segment has 7/2=3 ,
+we need extra 1 capacity , so add 1 at every segment 3+1=4 capacity ,
+so the absolute capacity will be 4*2=8
+*/
+
+type LRUCacheShard struct {
 	Cap  int
 	Map  map[int]*list.Element
 	List *list.List
 	Lock sync.RWMutex
+}
+
+type LRUCache struct {
+	Shard map[int]*LRUCacheShard
 }
 
 type pair struct {
@@ -17,37 +34,55 @@ type pair struct {
 }
 
 func Constructor(capacity int) LRUCache {
+	cpuNum := runtime.NumCPU()
+	shard := map[int]*LRUCacheShard{}
+	capSeg := capacity / cpuNum
+	// determine odd or even number
+	if capacity&cpuNum == 1 {
+		capSeg++
+	}
+	for i := 0; i < cpuNum; i++ {
+		value := &LRUCacheShard{
+			Cap:  capSeg,
+			Map:  map[int]*list.Element{},
+			List: list.New(),
+		}
+		shard[i] = value
+	}
 	return LRUCache{
-		Cap:  capacity,
-		Map:  make(map[int]*list.Element),
-		List: list.New(),
+		Shard: shard,
 	}
 }
 
+func (c *LRUCache) getShard(key int) *LRUCacheShard {
+	return c.Shard[key%runtime.NumCPU()]
+}
+
 func (c *LRUCache) Get(key int) int {
-	// 需移动头部节点，因此加写锁
-	c.Lock.Lock()
-	defer c.Lock.Unlock()
-	if el, ok := c.Map[key]; ok {
-		c.List.MoveToFront(el)
+	shard := c.getShard(key)
+	shard.Lock.RLock()
+	defer shard.Lock.RUnlock()
+	if el, ok := shard.Map[key]; ok {
+		shard.List.MoveToFront(el)
 		return el.Value.(pair).V
 	}
 	return -1
 }
 
 func (c *LRUCache) Put(key int, value int) {
-	c.Lock.Lock()
-	defer c.Lock.Unlock()
-	if el, ok := c.Map[key]; ok {
+	shard := c.getShard(key)
+	shard.Lock.Lock()
+	defer shard.Lock.Unlock()
+	if el, ok := shard.Map[key]; ok {
 		el.Value = pair{K: key, V: value}
-		c.List.MoveToFront(el)
+		shard.List.MoveToFront(el)
 	} else {
-		el := c.List.PushFront(pair{K: key, V: value})
-		c.Map[key] = el
+		el := shard.List.PushFront(pair{K: key, V: value})
+		shard.Map[key] = el
 	}
-	if c.List.Len() > c.Cap {
-		el := c.List.Back()
-		c.List.Remove(el)
-		delete(c.Map, el.Value.(pair).K)
+	if shard.List.Len() > shard.Cap {
+		el := shard.List.Back()
+		shard.List.Remove(el)
+		delete(shard.Map, el.Value.(pair).K)
 	}
 }
